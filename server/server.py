3bc64488533ef978f67137d831dc8cdabe105b04
songlin <songlin.lin@yunfangdata.com>
2018-08-03 17:17:50 +0800

    增加查询户外组织活动数量的汇总接口

--- server/server.py
+++ server/server.py
@@ -1,7 +1,8 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 from flask_restful import Api, Resource, reqparse
 
-from utils import HT, get_activity, get_activitys_by_page
+from utils import (HT, get_activity, get_activitys_by_page,
+                   get_activity_outdoor_agg)
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,6 +18,13 @@ def art():
     return content
 
 
+@app.route('/art_agg')
+def arg_agg():
+    before = int(request.args.get('before', 1))
+    results = get_activity_outdoor_agg(before)
+    return jsonify(list(results))
+
+
 class HelloWorld(Resource):
     def get(self):
         parser = reqparse.RequestParser()
@@ -25,13 +33,12 @@ class HelloWorld(Resource):
         parser.add_argument('before', type=int)
         parser.add_argument('key', type=str)
         args = parser.parse_args()
-        return list(get_activitys_by_page(args['page'],
-            args['pagesize'],
-            args['before'],
-            args['key']))
+        return list(
+            get_activitys_by_page(args['page'], args['pagesize'],
+                                  args['before'], args['key']))
+
 
 api.add_resource(HelloWorld, '/hello')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
